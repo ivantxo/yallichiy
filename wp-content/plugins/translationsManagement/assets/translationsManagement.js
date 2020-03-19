ba72e499739b6d9032432223368aae460a06c013
ivantxo <ivan.ramirezf@gmail.com>
2020-03-20 06:16:43 +1000

    Added Confirmation div and Confirmation actions (Cancel and Confirm)

--- wp-content/plugins/translationsManagement/assets/translationsManagement.js
+++ wp-content/plugins/translationsManagement/assets/translationsManagement.js
@@ -1,11 +1,23 @@
 jQuery(document).ready(function($) {
-  $('.request').click(function () {
+  var request = $('.request');
+  var translator = $('.translator');
+  var requestID = undefined;
+  var translatorID = undefined;
+
+  request.click(function () {
     $(this).css('background-color', '#EBDEF0');
-    console.log('id', $(this).data('id'));
+    requestID = $(this).attr('id');
+    console.log('id', requestID);
   });
 
-  $('.translator').click(function () {
+  translator.click(function () {
     $(this).css('background-color', '#FADBD8');
-    console.log('id', $(this).data('id'));
+    translatorID = $(this).attr('id');
+    console.log('id', translatorID);
+    if (typeof requestID !== 'undefined' && typeof translatorID !== 'undefined') {
+      $('#admin_confirm_request').html($('#' + requestID).html());
+      $('#admin_confirm_translator').html($('#' + translatorID).html());
+      $('#admin_confirm_actions').show();
+    }
   });
 });
--- wp-content/plugins/translationsManagement/templates/adminManagement.php
+++ wp-content/plugins/translationsManagement/templates/adminManagement.php
@@ -2,7 +2,7 @@
   <div id="admin_requests" style="height: 300px; overflow: auto;">
       <?php if (!empty($attributes['requests'])): ?>
           <?php foreach ($attributes['requests'] as $request): ?>
-            <div class="request" data-id="request_<?= $request->id; ?>" style="border: 2px groove #5f97a8;">
+            <div class="request" id="request_<?= $request->id; ?>" style="border: 2px groove #5f97a8;">
               <b><?php echo "Type: "; ?></b><label><?php echo $request->type; ?></label><br/>
               <b><?php echo "Source: "; ?></b><label><?php echo ucfirst($request->source_language); ?></label><br/>
               <b><?php echo "Target: "; ?></b><label><?php echo ucfirst($request->target_language); ?></label><br/>
@@ -14,7 +14,7 @@
             <br/><br/>
           <?php endforeach; ?>
           <?php foreach ($attributes['requests'] as $request): ?>
-            <div class="request" data-id="request_<?= $request->id; ?>" style="border: 2px groove #5f97a8;">
+            <div class="request" id="request2_<?= $request->id; ?>" style="border: 2px groove #5f97a8;">
               <b><?php echo "Type: "; ?></b><label><?php echo $request->type; ?></label><br/>
               <b><?php echo "Source: "; ?></b><label><?php echo ucfirst($request->source_language); ?></label><br/>
               <b><?php echo "Target: "; ?></b><label><?php echo ucfirst($request->target_language); ?></label><br/>
@@ -31,7 +31,7 @@
   <div id="admin_translators" style="height: 300px; overflow: auto;">
       <?php if (!empty($attributes['translators'])): ?>
           <?php foreach ($attributes['translators'] as $translator): ?>
-            <div class="translator" data-id="translator_<?= $translator->id?>" style="border: 2px groove #5f97a8;">
+            <div class="translator" id="translator_<?= $translator->id?>" style="border: 2px groove #5f97a8;">
               <b><?php echo "Name: "; ?></b><label><?php echo $translator->name . ' ' . $translator->surname; ?></label><br/>
               <b><?php echo "Phone: "; ?></b><label><?php echo $translator->phone; ?></label><br/>
               <b><?php echo "Email: : "; ?></b><label><?php echo $translator->email; ?></label><br/>
@@ -40,7 +40,7 @@
           <br/>
           <?php endforeach; ?>
           <?php foreach ($attributes['translators'] as $translator): ?>
-            <div class="translator" data-id="translator2_<?= $translator->id?>" style="border: 2px groove #5f97a8;">
+            <div class="translator" id="translator2_<?= $translator->id?>" style="border: 2px groove #5f97a8;">
               <b><?php echo "Name: "; ?></b><label><?php echo $translator->name . ' ' . $translator->surname; ?></label><br/>
               <b><?php echo "Phone: "; ?></b><label><?php echo $translator->phone; ?></label><br/>
               <b><?php echo "Email: : "; ?></b><label><?php echo $translator->email; ?></label><br/>
@@ -49,5 +49,19 @@
           <br/>
           <?php endforeach; ?>
       <?php endif; ?>
+  </div>
+</div>
+
+<br/><br/><br/>
+<div id="admin_confirm_requests">
+  <div>
+    <div style="display: flex; flex-direction: row; flex-wrap: wrap; width: 100%;">
+      <div id="admin_confirm_request"></div>
+      <div id="admin_confirm_translator"></div>
+    </div>
+    <div id="admin_confirm_actions" style="display: none; flex-direction: row; flex-wrap: wrap; width: 100%;">
+      <button value="Cancel">Cancel</button>
+      <button value="Confirm">Confirm</button>
+    </div>
   </div>
 </div>
